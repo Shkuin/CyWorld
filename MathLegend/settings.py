d6f8fa351375c828a1141405d5c492641c81c07a
Shkuin <bioniklsn123@mail.ru>
2020-07-01 14:44:39 +0500

    a

--- MathLegend/settings.py
+++ MathLegend/settings.py
@@ -14,7 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import databases as DATABASES
-from networkx.release import url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -82,8 +81,6 @@ WSGI_APPLICATION = 'MathLegend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
 
 DATABASES = {
     'default': {
